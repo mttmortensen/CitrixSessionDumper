5f53c1171de6736dc2ef17fb24f63c056f0feca1
mattm <mtt.mortensen527@gmail.com>
2025-06-16 12:54:18 -0600

    Add project files.

---
+++ CitrixSessionDumper/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CitrixSessionDumper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string timestamp = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
+            string username = Environment.UserName;
+            string domain = Environment.UserDomainName;
+            string machine = Environment.MachineName;
+        }
+    }
+}
