4b2a76aa9d77a51b5786201b08e5340b4d4c806d
mattm <mtt.mortensen527@gmail.com>
2025-06-24 10:59:11 -0600

    adding a log file path section for the dump. starting with it's own class

---
+++ CitrixSessionDumper/CitrixLogPaths.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CitrixSessionDumper
+{
+    // Provides methods to enumerate Citrix-related log paths on both client and VDA sides.
+    public static class CitrixLogPaths
+    {
+        // Enumerates common client-side Citrix log directories and their contents.
+        public static string GetClientSideLogs() { }
+
+        // Enumerates common VDA-side Citrix log directories and their contents.
+        public static string GetVDASideLogs() { }
+    }
+}
